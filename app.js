71feb025fa1c594470ed5314795890a9b5105936
imonweb <contact@imonweb.co.uk>
2021-09-04 01:10:15 +0100

    completed

--- app.js
+++ app.js
@@ -77,6 +77,9 @@ slideControl.onclick = () => {
 }
 
 openNav = () => {
-  let nav = document.querySelector('.nav-overlay')
-  nav.classList.
+  let nav = document.querySelector('.nav-overlay');
+  nav.classList.toggle('active')
+  let hamb = document.querySelector('.hamburger');
+  hamb.classList.toggle('active');
 }
+
